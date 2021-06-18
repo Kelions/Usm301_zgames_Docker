f33953b9ceb89748436f6109b9d6ab3f586d14c3
ArturoReyes <Arturo.reyes.recabarren@gmail.com>
2021-06-17 20:37:20 -0400

    Creacion de tablas en la BD,creacion de modelos en la BD.

--- web/zgames/app/Http/Controllers/ConsolasController.php
+++ web/zgames/app/Http/Controllers/ConsolasController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Consola;
+
 
 class ConsolasController extends Controller
 {
@@ -15,4 +16,25 @@ class ConsolasController extends Controller
         $marcas[] = "Sega";
         return $marcas;
     }
+
+## Esta funcion irea  abuscar todas las consolas de la BD y las va a retornar
+public function getConsolas(){
+    //equivalente a un select * from consolas
+    $consolas = Consola ::all();
+    return $consolas;
+
+}
+
+
+## Esta funcion registrara una consola de ejhemplo en la bd
+public function crearConsolas(){
+
+    //Equivalente a un insert
+    $consola = new Consola();
+    $consola->nombre = "nintendo Switch";
+    $consola->marca = "nintendo";
+    $consola->anio = 2015;
+
+    $consola->save();
+}
 }
--- web/zgames/routes/api.php
+++ web/zgames/routes/api.php
@@ -4,12 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
-
+quiero usar el controlador asi que lo importoo se importa con namespace\NombreClase
 */
 use App\Http\Controllers\ConsolasController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+// la ruta puede ser post o get (post para enviar cosas a la bd)
 Route::get("marcas/get", [ConsolasController::class,"getMarcas"]);
+
+Route::get("consolas/get",[ConsolasController::class,"getConsolas"]);
+
+Route::get("consolas/post",[ConsolasController::class,"crearConsolas"]);
